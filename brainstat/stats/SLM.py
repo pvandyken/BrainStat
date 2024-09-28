Fix off-by-one error

brainstat/stats/SLM.py
@@ -322,7 +322,9 @@ class SLM:
                         )
                         yeo_names, _ = fetch_yeo_networks_metadata(7)
                         yeo_names.insert(0, "Undefined")
-                        yeo7_index = yeo7[self.P["peak"]["vertid"][i]]
+                        if (idx := self.P["peak"]["vertid"][i]) is not None:
+                            idx -= 1
+                        yeo7_index = yeo7[idx]
                         if "yeo7" not in self.P["peak"]:
                             self.P["peak"]["yeo7"] = []
                         self.P["peak"]["yeo7"].append(np.take(yeo_names, yeo7_index))